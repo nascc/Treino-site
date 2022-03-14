cd6f2f0f85602b5dc182042414f7f1011c7b14f2
nascc <nasc.caio96@gmail.com>
2022-03-14 18:09:08 -0300

    Treinando prompt

---
+++ src/script.js
@@ -0,0 +1,9 @@
+var n1 = window.prompt("Salve meu bom. Fale um número pra gente somar... Lembre-se de usar ponto para separar decimais, caso você use números com decimais.");
+var n2 = window.prompt("Agora fale outro...");
+
+var soma2 = Number.parseFloat(n1) + Number.parseFloat(n2);
+
+window.alert(`A soma é: ${soma}`);
+
+// Separando as aulas.. 
+
